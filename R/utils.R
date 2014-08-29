8f993fded62c8c4ec64c741a2baeeae85548ed61
Raphael Gottardo <raph@rglab.org>
2014-08-28 22:07:16 -0700

    Change to arm_name
    Have to change this for the new default views.

--- R/utils.R
+++ R/utils.R
@@ -22,7 +22,7 @@ quick_plot <- function(dt, normalize_to_baseline=TRUE, type="auto", ...)
   if(length(grep("analyte",colnames(dt)))==0)
     dt <- dt[,analyte:=""]
   # Compute summaries over all repeated measures (e.g. multiple virus strains)
-  dt_unique <- dt[,list(response=mean(log2(value_reported), na.rm=TRUE)),by="arm_accession,subject_accession,analyte,study_time_collected"]
+  dt_unique <- dt[,list(response=mean(log2(value_reported), na.rm=TRUE)),by="arm_name,subject_accession,analyte,study_time_collected"]
   
   if(type=="auto" & length(unique(dt_unique$analyte))>5)
     type <- "heatmap"
@@ -33,21 +33,20 @@ quick_plot <- function(dt, normalize_to_baseline=TRUE, type="auto", ...)
   if(normalize_to_baseline==TRUE)
   {
     # Remove the time zero response
-    dt_unique <- dt_unique[,response:=response-response[study_time_collected==0],by="arm_accession,subject_accession,analyte"][study_time_collected!=0]
+    dt_unique <- dt_unique[,response:=response-response[study_time_collected==0],by="arm_name,subject_accession,analyte"][study_time_collected!=0]
   }
   
-  # TODO: change arm_accession with cohort, once it's there
   if(type=="boxplot")
   {
-    p <- qplot(as.factor(study_time_collected), response, data=dt_unique, facets=analyte~arm_accession,geom=c("boxplot","jitter"), ..., xlab = "time", ylab="response (log2)")
+    p <- qplot(as.factor(study_time_collected), response, data=dt_unique, facets=analyte~arm_name,geom=c("boxplot","jitter"), ..., xlab = "time", ylab="response (log2)")
   }
   else if(type=="line")
   {
-    p <- qplot(as.factor(study_time_collected), response, data=dt_unique, facets=analyte~arm_accession,geom=c("line","point"), ..., xlab = "time", ylab="response (log2)", group=subject_accession)  
+    p <- qplot(as.factor(study_time_collected), response, data=dt_unique, facets=analyte~arm_name,geom=c("line","point"), ..., xlab = "time", ylab="response (log2)", group=subject_accession)  
   }
   else if(type=="heatmap")
   {
-    p <- qplot(as.factor(study_time_collected), analyte, data=dt_unique, facets=~arm_accession,geom=c("raster"), ..., xlab = "time", fill=response) + scale_fill_gradient2(high = "#a50026", mid="#ffffbf", low="#313695")
+    p <- qplot(as.factor(study_time_collected), analyte, data=dt_unique, facets=~arm_name,geom=c("raster"), ..., xlab = "time", fill=response) + scale_fill_gradient2(high = "#a50026", mid="#ffffbf", low="#313695")
   }
   p+theme_minimal()
 }
