a54d638cbbf19fbbdb6df07e869f9f25d428217e
mikejiang <wjiang2@fhcrc.org>
2014-12-01 16:06:29 -0800

    update unit testing

--- tests/run-all.R
+++ tests/run-all.R
@@ -1,12 +1,10 @@
 library(testthat)
 library(ImmuneSpaceR)
-library(digest)
 
 labkey.url.base="https://www.immunespace.org" 
 labkey.url.path="Studies/SDY269"
 labkey.email.user="wjiang2@fhcrc.org"
 
 # internal unit tests
-#expectRes <- readRDS("~/rglab/workspace/ImmuneSpaceR/misc/expectRes.rds")
-#test_package("ImmuneSpaceR")
-
+#expectRes <<- readRDS("~/rglab/workspace/ImmuneSpaceR/misc/expectRes.rds")
+#test_file("~/rglab/workspace/ImmuneSpaceR/inst/tests/test-IScon.R")
