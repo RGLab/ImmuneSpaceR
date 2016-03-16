b0f3de4770b319d98bb64790052e3f0c300e049f
SRenan <renan.sauteraud@gmail.com>
2016-03-16 10:48:49 -0700

    Update SDY144's report to use virus
    Update theme_IS to work around ggplot2 v2 issues

--- R/utils.R
+++ R/utils.R
@@ -48,23 +48,30 @@ ISpalette <- function(n){
 #' 
 #' Theme that matches ImmuneSpace's graphic style.
 #' 
+#' @param base_size A \code{numeric}. Base font size.
+#' 
 #' @return A theme object
 #' 
 #' @importFrom ggplot2 theme theme_classic element_line element_text element_rect
 #' @importFrom ggplot2 continuous_scale rel
 #' @export
-theme_IS <- function(base_size) {
+theme_IS <- function(base_size = 12) {
   .override_scale()
-  theme_classic() +
+  #theme_classic() +
     theme(text = element_text(size = base_size)) +
     theme(axis.text.x = element_text(angle = 45)) +
-    theme(panel.background = element_rect(fill="#FDF6E3")) +
+    theme(panel.background = element_rect(fill = "#FDF6E3")) +
     theme(panel.grid.major = element_line(colour = "#ded8d5", linetype = "dotted")) +
-    theme(panel.grid.minor = element_line(colour = "#FDF6E3", linetype = "dotted")) +
-    theme(axis.ticks = element_line(size = 0)) +
-    theme(axis.line = element_line(size = 0.5, colour = "black")) +
+    theme(panel.grid.minor = element_blank()) +#line(colour = "#FDF6E3", linetype = "dotted")) +
+    #theme(panel.background = element_rect(fill = "#FDF6E3", colour = NA)) +
+    #theme(panel.grid.major = element_line(colour = "lightgray", linetype = "dotted")) +
+    #theme(panel.grid.minor = element_line(colour = "gray", linetype = "dotted")) +
+    theme(axis.ticks = element_blank()) +
+    #theme(axis.line.x = element_line(size = 5, colour = "black")) +
+    theme(axis.line.x = element_line(size = 0.5, colour = "black")) + 
+    theme(axis.line.y = element_line(size = 0.5, colour = "black")) +
     theme(plot.title = element_text(size = rel(1))) +
-    theme(strip.background = element_rect(colour = "white", fill = "white"))
+    theme(strip.background = element_rect(colour = "white", fill = "white")) #bg of facets
 }
 
 # Add scales that use a different color scheme to the environment.
