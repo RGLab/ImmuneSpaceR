aa58859a9a845c04d40324d51a960806ce4cbe64
mtmorgan@fhcrc.org <mtmorgan@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-04-25 09:23:22 +0000

    Adding MMDiff2, ImmuneSpaceR
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ImmuneSpaceR@116758 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/CreateConnection.R
@@ -0,0 +1,250 @@
+################################################################################
+# Main class and instantiation
+################################################################################
+
+#' @title CreateConnection
+#' @name CreateConnection
+#' @param study A \code{"character"} vector naming the study.
+#' @param verbose A \code{"logical"} whether to print the extra details for 
+#' troubleshooting. 
+#' @description Constructor for \code{ImmuneSpaceConnection} class
+#' @details Instantiates an \code{ImmuneSpaceConnection} for \code{study}
+#' The constructor will try to take the values of the various `labkey.*` 
+#' parameters from the global environment. If they don't exist, it will use 
+#' default values. These are assigned to `options`, which are then used by the 
+#' \code{ImmuneSpaceConnection} class.
+#' @export CreateConnection
+#' @return an instance of an \code{ImmuneSpaceConnection}
+#' @seealso ImmuneSpaceConnection
+#' @examples
+#' \dontrun{
+#'   # Single study 
+#'   con <- CreateConnection("SDY269")
+#'   # Cross study
+#'   con <- CreateConnection("")
+#' }
+#' 
+#' sdy <- try(CreateConnection("SDY269"))
+#' if(inherits(sdy, "try-error")){
+#'   print("Read the Introduction vignette for more information on how to set up
+#'   a .netrc file.")
+#' }
+CreateConnection = function(study = NULL, verbose = FALSE){
+  # Try to parse labkey options from global environment 
+  # which really should have been done through option()/getOption() mechanism
+  # Here we do this to be compatible to labkey online report system 
+  # that automatically assigns these variables in global environment
+  labkey.url.base <- try(get("labkey.url.base", .GlobalEnv), silent = TRUE)
+  if(inherits(labkey.url.base, "try-error"))
+    labkey.url.base <- "https://www.immunespace.org"
+  labkey.url.base <- gsub("http:", "https:", labkey.url.base)
+  if(length(grep("^https://", labkey.url.base)) == 0)
+    labkey.url.base <- paste0("https://", labkey.url.base)
+  labkey.user.email <- try(get("labkey.user.email", .GlobalEnv), silent = TRUE)
+  if(inherits(labkey.user.email, "try-error"))
+    labkey.user.email <- "unknown_user at not_a_domain.com"
+  
+  # set curoption for Rlabkey package
+  #
+  # Rlabkey stores the Curl options in its package environment through labkey.setCurlOptions call.
+  # So in theory we need to reset it prior to each Rlabkey query 
+  # because  multiple connections created by user indiviudally (not as ImmuneSystemConnectionList)
+  # may have different different urls and ssl settings. 
+  # (Ideally labkey.selectRows should optionally parse the options from its argument besides package environment)
+  # 
+  # for now we assume they all share the same setting and init it only once here
+  nf <- try(get("labkey.netrc.file", .GlobalEnv), silent = TRUE)
+  if(!inherits(nf, "try-error") && !is.null(nf)){
+    curlOptions <- labkey.setCurlOptions(ssl.verifyhost = 2, sslversion = 1, netrc.file = nf)
+  } else{
+    curlOptions <- labkey.setCurlOptions(ssl.verifyhost = 2, sslversion = 1)
+  }
+  
+  if(length(study) <= 1){
+    .CreateConnection(study = study
+                      , labkey.url.base = labkey.url.base
+                      , labkey.user.email = labkey.user.email
+                      , verbose = verbose
+                      , curlOptions = curlOptions
+                      )
+  } else{
+    stop("For multiple studies, use an empty string and filter the connection.")
+  }
+}
+
+
+.CreateConnection = function(study = NULL
+                             , labkey.url.base
+                             , labkey.user.email
+                             , curlOptions
+                             , verbose
+                             , ...){
+  labkey.url.path<-try(get("labkey.url.path",.GlobalEnv),silent=TRUE)
+  if(inherits(labkey.url.path,"try-error")){
+    if(is.null(study)){
+      stop("study cannot be NULL")
+    }
+    labkey.url.path <- paste0("/Studies/",study)
+  }else if(!is.null(study)){
+    labkey.url.path <- file.path(dirname(labkey.url.path),study)
+  }
+  config <- list(labkey.url.base = labkey.url.base,
+                  labkey.url.path = labkey.url.path,
+                  labkey.user.email = labkey.user.email,
+                  curlOptions = curlOptions,
+                  verbose = verbose)
+  
+  .ISCon(config = config)
+}
+
+#'@rdname ImmuneSpaceConnection-class
+#'@aliases
+#'ImmuneSpace
+#'ImmuneSpaceConnection
+#'ImmuneSpaceConnection-class
+#'getGEMatrix
+#'getDataset
+#'listDatasets
+#'getGEAnalysis
+#'listGEAnalysis
+#'addTrt
+#'EMNames
+#'quick_plot
+#'@title The ImmuneSpaceConnection class
+#'
+#'@description
+#'A connection respresents a study or a set of studies available on ImmuneSpace.
+#'It provides function to download and display the data within these studies.
+#'
+#'@field study A \code{character}. The study accession number. Use an empty
+#'string ("") to create a connection at the project level.
+#'@field config A \code{list}. Stores configuration of the connection object 
+#'such as URL, path and username.
+#'@field available_datasets A \code{data.table}. The table of datasets available
+#'in the connection object.
+#'@field data_cache A \code{list}. Stores the data to avoid downloading the same
+#'tables multiple times.
+#'@field constants A \code{list}. Used to store information regarding 
+#'gene-expression data.
+#'
+#'@details
+#' Uses global variables \code{labkey.url.base}, and \code{labkey.url.path}, to
+#' access a study. \code{labkey.url.base} should be
+#' \code{https://www.immunespace.org/}. \code{labkey.url.path} should be
+#' \code{/Studies/studyname}, where 'studyname' is the accession number of the
+#' study.
+#' The ImmunespaceConnection will initialize itself, and look for a
+#' \code{.netrc} file in \code{"~/"} the user's home directory. The
+#' \code{.netrc} file should contain a \code{machine}, \code{login}, and
+#' \code{password} entry to allow access to ImmuneSpace, where \code{machine} is
+#' the host name like "www.immunespace.org".
+#' 
+#' @seealso 
+#'  \code{\link{CreateConnection}}
+#'  \code{\link{ImmuneSpaceR-package}} 
+#' @exportClass ImmuneSpaceConnection
+#' @examples
+#' \dontrun{
+#'   sdy269 <- CreateConnection("SDY269")
+#'   sdy269
+#' }
+#'@return An instance of an ImmuneSpaceConnection for a study in `labkey.url.path`
+.ISCon <- setRefClass(Class = "ImmuneSpaceConnection",
+            fields = list(study = "character", config = "list",
+                          available_datasets = "data.table",
+                          data_cache = "list", constants = "list")
+)
+
+# Functions used in initialize need to be declared ahead of it
+#' @importFrom gtools mixedsort
+.ISCon$methods(
+  checkStudy=function(verbose = FALSE){
+    validStudies <- mixedsort(grep("^SDY", basename(lsFolders(getSession(config$labkey.url.base, "Studies"))), value = TRUE))
+    req_study <- basename(config$labkey.url.path)
+    if(!req_study %in% c("", validStudies)){
+      if(!verbose){
+        stop(paste0(req_study, " is not a valid study"))
+      } else{
+        stop(paste0(req_study, " is not a valid study\nValid studies: ",
+                    paste(validStudies, collapse=", ")))
+      }
+    }
+  }
+)
+
+.ISCon$methods(
+  getAvailableDataSets=function(){
+    if(length(available_datasets)==0){
+      df <- labkey.selectRows(baseUrl = config$labkey.url.base
+                        , config$labkey.url.path
+                        , schemaName = "study"
+                        , queryName = "ISC_study_datasets")
+      available_datasets <<- data.table(df)#[,list(Label,Name,Description,`Key Property Name`)]
+    }
+  }
+)
+
+.ISCon$methods(
+  GeneExpressionMatrices=function(verbose = FALSE){
+    if(!is.null(data_cache[[constants$matrices]])){
+      data_cache[[constants$matrices]]
+    }else{
+      if(verbose){
+        ge <- try(data.table(
+          labkey.selectRows(baseUrl = config$labkey.url.base,
+                            config$labkey.url.path,
+                            schemaName = "assay.ExpressionMatrix.matrix",
+                            queryName = "Runs",
+                            colNameOpt = "fieldname",
+                            showHidden = TRUE,
+                            viewName = "expression_matrices")),
+        silent = TRUE)
+      } else {
+        suppressWarnings(
+          ge <- try(data.table(
+            labkey.selectRows(baseUrl = config$labkey.url.base,
+                              config$labkey.url.path,
+                              schemaName = "assay.ExpressionMatrix.matrix",
+                              queryName = "Runs",
+                              colNameOpt = "fieldname",
+                              showHidden = TRUE,
+                              viewName = "expression_matrices")),
+          silent = TRUE)
+        )
+      }
+      if(inherits(ge, "try-error") || nrow(ge) == 0){
+        #No assay or no runs
+        message("No gene expression data")
+        data_cache[[constants$matrices]] <<- NULL
+      } else{
+        setnames(ge,.self$.munge(colnames(ge)))
+        data_cache[[constants$matrices]]<<-ge
+      }
+    }
+    return(data_cache[[constants$matrices]])
+  }
+)
+
+.ISCon$methods(
+  initialize=function(..., config = NULL){
+    
+    #invoke the default init routine in case it needs to be invoked 
+    #(e.g. when using $new(object) to construct the new object based on the exiting object)
+    callSuper(...)
+    
+    constants <<- list(matrices="GE_matrices",matrix_inputs="GE_inputs")
+    
+    if(!is.null(config))
+      config <<- config
+
+    study <<- basename(config$labkey.url.path)
+    if(config$verbose){
+      checkStudy(config$verbose)
+    }
+    
+    getAvailableDataSets()
+
+    gematrices_success <- GeneExpressionMatrices(verbose = FALSE)
+    
+  }
+)
---
+++ R/getGEMatrix.R
@@ -0,0 +1,287 @@
+#' @include ImmuneSpace.R
+NULL
+
+
+#' @importFrom RCurl getCurlHandle curlPerform basicTextGatherer
+.ISCon$methods(
+  downloadMatrix=function(x, summary = FALSE){
+    cache_name <- paste0(x, ifelse(summary, "_sum", ""))
+    if(is.null(data_cache[[cache_name]])){
+      if(nrow(subset(data_cache[[constants$matrices]], name%in%x)) == 0){
+        stop(sprintf("No matrix %s in study\n", x))
+      }
+      summary <- ifelse(summary, ".summary", "")
+      if(config$labkey.url.path == "/Studies/"){
+        path <- paste0("/Studies/", data_cache$GE_matrices[name == x, folder], "/")
+      } else{
+        path <- gsub("/$","",config$labkey.url.path)
+      }
+      link <- URLdecode(file.path(gsub("http:","https:", gsub("/$","",config$labkey.url.base)),
+                                  "_webdav",  path, "@files/analysis/exprs_matrices",
+                                  paste0(x, ".tsv", summary)))
+      localpath <- .self$.localStudyPath(link)
+      if(.self$.isRunningLocally(localpath)){
+        fl <- localpath
+        message("Reading local matrix")
+        data_cache[[cache_name]] <<- fread(fl, header = TRUE)
+      }else{
+        opts <- config$curlOptions
+        opts$netrc <- 1L
+        #opts$httpauth <- 1L
+        handle <- getCurlHandle(.opts=opts)
+        h <- basicTextGatherer()
+        message("Downloading matrix..")
+        curlPerform(url = link, curl = handle, writefunction = h$update)
+        fl <- tempfile()
+        write(h$value(), file = fl)
+        EM <- fread(fl, header = TRUE)
+        if(nrow(EM) == 0){
+          stop("The downloaded matrix has 0 rows. Something went wrong")
+        }
+        data_cache[[cache_name]] <<- EM
+        file.remove(fl)
+      }
+      
+    }else{
+      data_cache[[cache_name]]
+    }
+  }
+)
+
+.ISCon$methods(
+  GeneExpressionFeatures=function(matrix_name,summary=FALSE){
+    cache_name <- paste0(matrix_name, ifelse(summary, "_sum", ""))
+    if(!matrix_name %in% data_cache[[constants$matrices]][, name]){
+      stop("Invalid gene expression matrix name");
+    }
+    annotation_set_id <- .self$.getFeatureId(matrix_name)
+    if(is.null(data_cache[[.self$.mungeFeatureId(annotation_set_id)]])){
+      if(!summary){
+        message("Downloading Features..")
+        featureAnnotationSetQuery = sprintf("SELECT * from FeatureAnnotation
+                                            where FeatureAnnotationSetId='%s';",
+                                            annotation_set_id);
+        features <- labkey.executeSql(config$labkey.url.base, 
+                                      config$labkey.url.path,
+                                      schemaName = "Microarray",
+                                      sql = featureAnnotationSetQuery,
+                                      colNameOpt = "fieldname")
+        setnames(features, "GeneSymbol", "gene_symbol")
+      }else{
+        features <- data.frame(FeatureId=data_cache[[cache_name]][,gene_symbol],
+                             gene_symbol=data_cache[[cache_name]][,gene_symbol])
+      }
+      data_cache[[.self$.mungeFeatureId(annotation_set_id)]] <<- features
+    }
+  }
+)
+
+.ISCon$methods(
+  ConstructExpressionSet=function(matrix_name, summary){
+    cache_name <- paste0(matrix_name, ifelse(summary, "_sum", ""))
+    #matrix
+    message("Constructing ExpressionSet")
+    matrix <- data_cache[[cache_name]]
+    #features
+    features <- data_cache[[.self$.mungeFeatureId(.self$.getFeatureId(matrix_name))]][,c("FeatureId","gene_symbol")]
+    
+    runID <- data_cache$GE_matrices[name == matrix_name, rowid]
+    pheno_filter <- makeFilter(c("Run", "EQUAL", runID), 
+                               c("Biosample/biosample_accession", "IN", paste(colnames(matrix), collapse = ";")))
+    pheno <- unique(data.table(labkey.selectRows(
+      config$labkey.url.base, config$labkey.url.path,
+      #"assay.ExpressionMatrix.matrix", "InputSamples", "gene_expression_matrices",
+      "study", "HM_InputSamplesQuerySnapshot",
+      containerFilter = "CurrentAndSubfolders",
+      colNameOpt = "caption", colFilter = pheno_filter)))
+    
+    setnames(pheno, .self$.munge(colnames(pheno)))
+
+    #pheno <- pheno[, list(biosample_accession, ParticipantId, arm_name,
+    pheno <- pheno[, list(biosample_accession, participant_id, cohort,
+                          study_time_collected, study_time_collected_unit)]
+    
+    if(summary){
+      fdata <- data.frame(FeatureId = matrix$gene_symbol, gene_symbol = matrix$gene_symbol, row.names = matrix$gene_symbol)
+      rownames(fdata) <- fdata$FeatureId
+      fdata <- AnnotatedDataFrame(fdata)
+    } else{
+      try(setnames(matrix, " ", "FeatureId"), silent = TRUE)
+      try(setnames(matrix, "V1", "FeatureId"), silent = TRUE)
+      fdata <- data.table(FeatureId = as.character(matrix$FeatureId))
+      fdata <- merge(fdata, features, by = "FeatureId", all.x = TRUE)
+      fdata <- as.data.frame(fdata)
+      rownames(fdata) <- fdata$FeatureId
+      fdata <- AnnotatedDataFrame(fdata)
+    }
+    dups <- colnames(matrix)[duplicated(colnames(matrix))]
+    if(length(dups) > 0){
+      for(dup in dups){
+        dupIdx <- grep(dup, colnames(matrix))
+        newNames <- paste0(dup, 1:length(dupIdx))
+        setnames(matrix, dupIdx, newNames)
+        eval(substitute(matrix[, `:=`(dup, rowMeans(matrix[, dupIdx, with = FALSE]))], list(dup = dup)))
+        eval(substitute(matrix[, `:=`(newNames, NULL)], list(newNames = newNames)))
+      }
+      if(.self$config$verbose){
+        warning("The matrix contains subjects with multiple measures per timepoint. Averaging expression values.")
+      }
+    }
+    exprs <- as.matrix(matrix[, -1L, with = FALSE])
+    exprs <- exprs[, colnames(exprs) %in% pheno$biosample_accession] #At project level, InputSamples may be filtered
+
+    pheno <- data.frame(pheno)
+    rownames(pheno) <- pheno$biosample_accession
+    pheno <- pheno[colnames(exprs), ]
+    ad_pheno <- AnnotatedDataFrame(data = pheno)
+    es <- ExpressionSet(assayData = exprs,
+                        phenoData = ad_pheno, featureData = fdata)
+    data_cache[[cache_name]] <<- es
+  }
+)
+
+# Downloads a normalized gene expression matrix from ImmuneSpace.
+.ISCon$methods(
+  getGEMatrix=function(x = NULL, cohort = NULL, summary = FALSE, reload=FALSE){
+    "Downloads a normalized gene expression matrix from ImmuneSpace.\n
+    `x': A `character'. The name of the gene expression matrix to download.\n
+    `cohort': A `character'. The name of a cohort that has an associated gene
+    expression matrix. Note that if `cohort' isn't NULL, then `x' is ignored.
+    `summary': A `logical'. If set to TRUE, Downloads a matrix with expression
+    averaged by gene symbol.
+    `reload': A `logical'. If set to TRUE, the matrix will be downloaded again,
+    even if a cached cop exist in the ImmuneSpaceConnection object."
+    cohort_name <- cohort #can't use cohort = cohort in d.t
+    if(!is.null(cohort_name)){
+      if(all(cohort_name %in% data_cache$GE_matrices$cohort)){
+        x <- data_cache$GE_matrices[cohort %in% cohort_name, name]
+      } else{
+        validCohorts <- data_cache$GE_matrices[, cohort]
+        stop(paste("No expression matrix for the given cohort.",
+                   "Valid cohorts:", paste(validCohorts, collapse = ", ")))
+      }
+    }
+    cache_name <- paste0(x, ifelse(summary, "_sum", ""))
+    if(length(x) > 1){
+      data_cache[cache_name] <<- NULL
+      lapply(x, downloadMatrix, summary)
+      lapply(x, GeneExpressionFeatures,summary)
+      lapply(x, ConstructExpressionSet, summary)
+      ret <- .combineEMs(data_cache[cache_name])
+      if(dim(ret)[[1]] == 0){
+        # No features shared
+        warn <- "The returned ExpressionSet has 0 rows. No feature is shared accross the selected runs or cohorts."
+        if(!summary){
+          warn <- paste(warn, "Try setting summary to TRUE to merge the matrices by gene symbol.")
+        }
+        warning(warn)
+      }
+      return(ret)
+    } else{
+      if (cache_name %in% names(data_cache) && !reload) {
+        data_cache[[cache_name]]
+      }
+      else {
+        data_cache[[cache_name]] <<- NULL
+        downloadMatrix(x, summary)
+        GeneExpressionFeatures(x, summary)
+        ConstructExpressionSet(x, summary)
+        data_cache[[cache_name]]
+      }
+    }
+  }
+)
+
+# Combine EMs and output only genes available in all EMs.
+#' @importFrom Biobase fData
+.combineEMs <- function(EMlist){
+  message("Combining ExpressionSets")
+  fds <- lapply(EMlist, function(x){ droplevels(data.table(fData(x)))})
+  fd <- Reduce(f = function(x, y){ merge(x, y, by = c("FeatureId", "gene_symbol"))}, fds)
+  EMlist <- lapply(EMlist, "[", as.character(fd$FeatureId))
+  for(i in 1:length(EMlist)){ fData(EMlist[[i]]) <- fd}
+  res <- Reduce(f = combine, EMlist)
+}
+
+
+# Add treatment information to the phenoData of an expression matrix available in the connection object.
+.ISCon$methods(
+  addTrt=function(x = NULL){
+    "Add treatment information to the phenoData of an expression matrix
+     available in the connection object.\n
+    x: A character. The name of a expression matrix that has been downloaded 
+    from the connection."
+    if(is.null(x) | !x %in% names(data_cache)){
+      stop(paste(x, "is not a valid expression matrix."))
+    } 
+    bsFilter <- makeFilter(c("biosample_accession", "IN",
+                             paste(pData(data_cache[[x]])$biosample_accession, collapse = ";")))
+    bs2es <- data.table(labkey.selectRows(config$labkey.url.base, config$labkey.url.path,
+                                          "immport", "biosample_2_expsample",
+                                          colFilter = bsFilter, colNameOpt = "rname"))
+    esFilter <- makeFilter(c("expsample_accession", "IN",
+                             paste(bs2es$expsample_accession, collapse = ";")))
+    es2trt <- data.table(labkey.selectRows(config$labkey.url.base, config$labkey.url.path,
+                                           "immport", "expsample_2_treatment",
+                                           colFilter = esFilter, colNameOpt = "rname"))
+    trtFilter <- makeFilter(c("treatment_accession", "IN",
+                              paste(es2trt$treatment_accession, collapse = ";")))
+    trt <- data.table(labkey.selectRows(config$labkey.url.base, config$labkey.url.path,
+                                        "immport", "treatment",
+                                        colFilter = trtFilter, colNameOpt = "rname"))
+    bs2trt <- merge(bs2es, es2trt, by = "expsample_accession")
+    bs2trt <- merge(bs2trt, trt, by = "treatment_accession")
+    pData(data_cache[[x]])$treatment <<- bs2trt[match(pData(data_cache[[x]])$biosample_accession, biosample_accession), name]
+    return(data_cache[[x]])
+  }
+)
+
+.ISCon$methods(
+  .getFeatureId=function(matrix_name){
+    subset(data_cache[[constants$matrices]],name%in%matrix_name)[, featureset]
+  }
+)
+
+.ISCon$methods(
+  .mungeFeatureId=function(annotation_set_id){
+    return(sprintf("featureset_%s",annotation_set_id))
+  }
+)
+
+
+#' @importFrom Biobase pData sampleNames
+.ISCon$methods(
+  EMNames=function(EM = NULL, colType = "participant_id"){
+    "Change the sampleNames of an ExpressionSet fetched by getGEMatrix using the
+    information in the phenodData slot.\n
+    x: An ExpressionSet, as returned by getGEMatrix.\n
+    colType: A character. The type of column names. Valid options are 'expsample_accession'
+    and 'participant_id'."
+    if(is.null(EM) | !is(EM, "ExpressionSet")){
+      stop("EM should be a valid ExpressionSet, as returned by getGEMatrix")
+    }
+    if(!all(grepl("^BS", sampleNames(EM)))){
+      stop("All sampleNames should be biosample_accession, as returned by getGEMatrix")
+    }
+    pd <- data.table(pData(EM))
+    colType <- gsub("_.*$", "", tolower(colType))
+    if(colType == "expsample"){
+      bsFilter <- makeFilter(c("biosample_accession", "IN",
+                                 paste(pd$biosample_accession, collapse = ";")))
+      bs2es <- data.table(labkey.selectRows(config$labkey.url.base, config$labkey.url.path,
+                                              "immport", "biosample_2_expsample",
+                                              colFilter = bsFilter, colNameOpt = "rname"))
+      pd <- merge(pd, bs2es[, list(biosample_accession, expsample_accession)], by = "biosample_accession")
+      es <- pd[match(sampleNames(EM), pd$biosample_accession), expsample_accession]
+      sampleNames(EM) <- pData(EM)$expsample_accession <- es
+    } else if(colType %in% c("participant", "subject")){
+      pd <- pd[, nID := paste0(participant_id, "_", tolower(substr(study_time_collected_unit, 1, 1)), study_time_collected)]
+      sampleNames(EM) <- pd[ match(sampleNames(EM), pd$biosample_accession), nID]
+    } else if(colType == "biosample"){
+      warning("Nothing done, the column names should already be biosample_accession numbers.")
+    } else{
+      stop("colType should be one of 'expsample_accession', 'biosample_accession', 'participant_id'.")
+    }
+    return(EM)
+  }
+)
