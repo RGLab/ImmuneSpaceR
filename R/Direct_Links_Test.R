2df1299d570f737dcede2289c89b5f7be91e1575
ehfhcrc <ehenrich@fredhutch.org>
2016-11-09 16:24:42 -0800

    "Working on faster bad link finding script"

--- R/Direct_Links_Test.R
+++ R/Direct_Links_Test.R
@@ -3,6 +3,40 @@
 # Last Updated: 11/8/2016
 
 # Purpose: Ensure all links work correctly
+require(plyr)
+require(grid)
+require(gridExtra)
+require(httr)
+
+
+#splitting function for SDYID
+get_sdyid <- function(subjectid){
+  id_table <- read.table(text = subjectid, sep = ".", as.is = T, fill = T)
+  sdyid <- id_table[1,2]
+}
+
+# test link function
+test_link <- function(study_id,filename,link_text){
+  
+  if(is.na(filename)){
+    result <- "NA"
+  }else if(filename == "<NA>"){
+    result <- "NA"
+  }else{
+    link <- paste0("https://www.immunespace.org/_webdav/Studies/SDY",study_id,"/@files/rawdata/",link_text,"/",filename) 
+    tryCatch({
+      status <- GET(link)
+      result <- status$status_code
+    },
+    error = function(e){
+      result <- "Error Occurred in GET(link)"
+    },
+    warning = function(w){
+      message(w)
+    })
+  }
+  return(result)
+}
 
 # Pull data from labkey
 get_data <- function(filetype, filename_col,link_text){
@@ -12,26 +46,38 @@ get_data <- function(filetype, filename_col,link_text){
                          queryName = filetype,
                          colNameOpt = "caption")
   
-  #iterate through the df to check link
-  for(i in df){
+  # get SDY ids into a table
+  Sdyids <- apply(MARGIN=1, df, FUN = function(x) get_sdyid(x))
   
-  # pull out sdy ID as by splitting participant ID
-  SDYid <- i$participant_id.split(".")[1]
-  filename <- i$file_column
+  # get filenames into separate table / list
+  Filenames <- df[filename_col]
   
-  # build link
-  link <- paste0("https://www.immunespace.org/_webdav/Studies/SDY",SDYid,"/@files/rawdata/",link_text,"/",filename)
+  # test link and output to temp table
+  Results <- mapply(test_link, Sdyids, Filenames, link_text)
   
-  #test_link
-  url.exists(link)
+  # merge temp table with comb df as third column
+  #final_df <- cbind(Sdyids,Filenames,Results)
   
-  # if the link fails, keep track of it in error log
-  }
+  # return table
+  return(Results)
 }
+
   
 
-get_data("fcs_sample_files","File Info Name","fcs")
-get_data("gene_expression_files","File Info Name","gene_expression")
+#fcs_table <- get_data("fcs_sample_files","File Info Name","fcs")
+gene_expr_table <- get_data("gene_expression_files","File Info Name","gene_expression")
+
+#sorted_ge <- gene_expr_table[order(Results),]
+#sort by result
 
 #TODO: figure out protocols ... different method of data extraction?
+
+# Print tables to pdf
+ts <-paste(format(Sys.time(), "%Y_%m_%d %T"), "pdf", sep = ".")
+pdfname <- paste0("Test_IS_Links_", ts)
+pdf(pdfname)
+grid.table(gene_expr_table)
+dev.off()
+
+
 
--- R/Test_all_studies.R
+++ R/Test_all_studies.R
@@ -16,33 +16,46 @@ sink(filename)
 
 # Hit LK DB to get list of available studies
 labkey.url.base <- "https://www.immunespace.org"
-folders <- labkey.getFolders(baseUrl = labkey.url.base, folderPath = "/Studies")
-SDY_list <- folders[1]
-SDY_list <- SDY_list[SDY_list != "Studies"]
-SDY_list <- SDY_list[SDY_list != "SDY_template"]
+#folders <- labkey.getFolders(baseUrl = labkey.url.base, folderPath = "/Studies")
+#SDY_list <- folders[1]
+#SDY_list <- SDY_list[SDY_list != "Studies"]
+#SDY_list <- SDY_list[SDY_list != "SDY_template"]
 
 # Start Time
 starttime <- strftime(Sys.time(),format = "%T")
 cat(starttime)
 cat("\n")
 
+# Open connection
+con <- CreateConnection("")
+tryCatch({
+  con$.test_files(mc=TRUE)
+  },
+  error = function(e){
+    message(e)
+  },
+  warning = function(w){
+    message(w)
+  })
+
+
 # Loop through all studies using RSauteraud's script(See ln133>ln179 in ImmuneSpace.R)
-for(i in SDY_list){
-  cat("Working on Study: ", i)
-  cat("\n")
-  con <- CreateConnection(i)
-  tryCatch({
-    con$.test_files(mc=TRUE)
-    },
-    error = function(e){
-      message(e)
-    },
-    warning = function(w){
-      message(w)
-    })
-  cat("\n")
-  cat("\n")
-}
+#for(i in SDY_list){
+#  cat("Working on Study: ", i)
+#  cat("\n")
+#  con <- CreateConnection(i)
+#  tryCatch({
+#    con$.test_files(mc=TRUE)
+#  },
+#  error = function(e){
+#    message(e)
+#  },
+#  warning = function(w){
+#    message(w)
+#  })
+#  cat("\n")
+#  cat("\n")
+#}
 
 # End Time
 endtime <- strftime(Sys.time(),format = "%T")
