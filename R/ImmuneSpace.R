95a321756ac14f67ae977ad862c01a55a88a7aba
Ju Yeong Kim <jkim2345@fredhutch.org>
2016-12-08 17:02:57 -0800

    Update .test_files method by parallezing instead of using forloops

--- R/ImmuneSpace.R
+++ R/ImmuneSpace.R
@@ -132,37 +132,42 @@
 # are properly loaded and accessible to the users.
 #' @importFrom RCurl getURL url.exists
 #' @importFrom rjson fromJSON
+#' @importFrom parallel mclapply detectCores
 .ISCon$methods(
   .test_files=function(what = c("gene_expression_files", "fcs_sample_files", "protocols")){
+    list_files <- function(link){
+      response <- NULL
+      if (url.exists(url = link, netrc = TRUE)){
+        response_raw <- getURL(url = link, netrc = TRUE)
+        response_json <- fromJSON(response_raw)
+        response <- unlist(lapply(response_json$files, function(x) x$text))
+      }
+      response
+    }
+    
     check_links <- function (name1, name2){
       res <- data.frame(file_info_name = NULL, study_accession = NULL, 
                         file_link = NULL, file_exists = NULL, 
-                        stringsAsFactors = F)
+                        stringsAsFactors = FALSE)
       
       if (name1 %in% studies$available_datasets$Name){
-        gef <- .self$getDataset(name1, original_view = TRUE)
-        gef <- gef[!is.na(file_info_name)]
-        gef <- unique(gef[, list(study_accession, file_info_name)])
-        for (i in unique(gef$study_accession)) {
-          folder_link <- paste0(.self$config$labkey.url.base, "/_webdav/Studies/", i, 
-                                "/%40files/rawdata/", name2, "?method=JSON")
-          
-          temp <- subset(gef, study_accession == i)
-          temp$file_link <- paste0(.self$config$labkey.url.base, "/_webdav/Studies/", i,
-                                   "/%40files/rawdata/", name2, "/", 
-                                   sapply(temp$file_info_name, URLencode))
-          
-          if (url.exists(url = folder_link, netrc = T)){
-            response_raw <- getURL(url = folder_link, netrc = T)
-            response_json <- fromJSON(response_raw)
-            response <- unlist(lapply(response_json$files, function(x) x$text))
-            
-            temp$file_exists <- temp$file_info_name %in% response
-          } else {
-            temp$file_exists <- F
-          }
-          res <- rbind(res, temp)
-        }
+        temp <- .self$getDataset(name1, original_view = TRUE)
+        temp <- temp[!is.na(file_info_name)]
+        temp <- unique(temp[, list(study_accession, file_info_name)])
+        
+        file_link <- paste0(.self$config$labkey.url.base, "/_webdav/Studies/", 
+                            temp$study_accession, "/%40files/rawdata/", name2, "/", 
+                            sapply(temp$file_info_name, URLencode))
+        
+        studies <- unique(temp$study_accession)
+        folder_link <- paste0(.self$config$labkey.url.base, "/_webdav/Studies/", 
+                              studies, "/%40files/rawdata/", name2, "?method=JSON")
+        
+        file_list <- unlist(mclapply(folder_link, list_files, mc.cores = detectCores()))
+        
+        file_exists <- temp$file_info_name %in% file_list
+        res <- data.frame(study = temp$study_accession, file_link = file_link, file_exists = file_exists, 
+                          stringsAsFactors = FALSE) 
         print(paste0(sum(res$file_exists), "/", nrow(res), " ", name1, " with valid links."))
       }
       
@@ -179,9 +184,6 @@
       ret$fcs_sample_files <- check_links("fcs_sample_files", "flow_cytometry")
     }
     if("protocols" %in% what){
-      res <- data.frame(study = NULL, file_link = NULL, file_exists = NULL, 
-                        stringsAsFactors = F)
-      
       if(.self$.isProject()){
         folders_list <- labkey.getFolders(baseUrl = .self$config$labkey.url.base, 
                                           folderPath = "/Studies/")
@@ -191,16 +193,12 @@
         folders <- basename(.self$config$labkey.url.path)
       }
       
-      for (folder in folders){
-        file_link <- paste0(.self$config$labkey.url.base, "/_webdav/Studies/", folder,
-                            "/%40files/protocols/", folder, "_protocol.zip")
-        file_exists <- url.exists(file_link, netrc = TRUE)
-        res <- rbind(res, data.frame(study = folder, 
-                                     file_link = file_link, 
-                                     file_exists = file_exists, 
-                                     stringsAsFactors = F))
-      }
+      file_link <- paste0(.self$config$labkey.url.base, "/_webdav/Studies/", folders, 
+                              "/%40files/protocols/", folders, "_protocol.zip")
+      file_exists <- unlist(mclapply(file_link, url.exists, netrc = TRUE, mc.cores = detectCores()))
       
+      res <- data.frame(study = folders, file_link = file_link, file_exists = file_exists, 
+                        stringsAsFactors = FALSE)      
       print(paste0(sum(res$file_exists), "/", nrow(res), " protocols with valid links."))
       ret$protocols <- res
     }
