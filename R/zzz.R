02935fc12ca6ece1bc3dea43f99d991d3c777378
Ju Yeong Kim <jkim2345@fredhutch.org>
2018-03-26 12:23:14 -0700

    Remove setting of RCurlOptions cainfo and CA certificates as it is no longer needed for httr

--- R/zzz.R
+++ R/zzz.R
@@ -5,15 +5,3 @@
     packageStartupMessage("A .netrc file is required to connect to ImmuneSpace. For more information on how to create one, refer to the Configuration section of the introduction vignette.")
   }
 }
-
-.onLoad <- function(libname, pkgname) {
-  if (.Platform$OS.type == "windows") {
-    options(RCurlOptions = list(cainfo = system.file("ssl_certs/cacert.pem", package = pkgname)))
-  }
-}
-
-.onUnload <- function(libname, pkgname) {
-  if (.Platform$OS.type == "windows") {
-    options(RCurlOptions = NULL)
-  }
-}
