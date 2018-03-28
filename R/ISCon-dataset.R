a6c0a067edfdf16df9cabaece9e75713a97fa722
lakikowolfe <lwolfe@fredhutch.org>
2018-03-28 16:05:20 -0700

    fix paste0 error

--- R/ISCon-dataset.R
+++ R/ISCon-dataset.R
@@ -41,10 +41,10 @@ ISCon$set(
   name = "getDataset",
   value = function(x, original_view = FALSE, reload = FALSE, colFilter = NULL, ...) {
     if (nrow(self$availableDatasets[Name %in% x]) == 0) {
-      wstring <- paste0(study, " ", x, " dataset is empty \ndata frame with 0 cols and 0 rows")
-      if (config$verbose) {
+      wstring <- paste0(self$study, " ", x, " dataset is empty \ndata frame with 0 cols and 0 rows")
+      if (self$config$verbose) {
         wstring <- paste0(wstring, "\n",
-                          "Valid datasets for ", study, ": ",
+                          "Valid datasets for ", self$study, ": ",
                           paste(self$availableDatasets$Name, collapse = ", "), ".")
       }
       warning(wstring)
